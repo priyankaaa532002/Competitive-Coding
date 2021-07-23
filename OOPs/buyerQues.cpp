366aca48b2f443da58fdc963f8baa2e938010c07
Priyanka <priyanka.chowdhury2020@vitstudent.ac.in>
2021-07-23 09:21:55 +0530

    init2

--- OOPs/buyerQues.cpp
+++
@@ -1,7 +0,0 @@
-1. Make a base class for buyer
-2. Make protected members id, name , phone numbder and address.
-3. Make public member to get the data.
-4. Make 2 other classes for ideal buyer and regular buyer , and make them inherit from the buyer class.
-5. Take input of items using a for loop and increase the price during every itheration for the regular buyer.
-6. In case of ideal buyer reduce price of items by 2% if item costs more than 5000 and 3% if items cost more than 7000.
-7. Finally print the total price according to the type of buyer.
